935fcbf9277b7634d257c7f008ba3ce1c961f920
spanchhi <Sucheta_Panchhi@intuit.com>
2018-08-11 04:28:05 +0530

    Suchet changes

--- app/src/main/java/com/start/neighbourfood/pages/SignupActivity.java
+++ app/src/main/java/com/start/neighbourfood/pages/SignupActivity.java
@@ -2,6 +2,8 @@ package com.start.neighbourfood.pages;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.neighbourfood.start.neighbourfood.R;
 
@@ -11,5 +13,14 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Spinner spinner = (Spinner) findViewById(R.id.apartment_dropdown_list);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.apartments_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }

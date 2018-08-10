424a2dd12c01b3ffcd952069cc9b897ec4f6db34
bob <mitin.dixit@hotmail.com>
2018-08-11 02:54:12 +0530

    added sign up activity

---
+++ app/src/main/java/com/start/neighbourfood/pages/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.start.neighbourfood.pages;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.neighbourfood.start.neighbourfood.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
